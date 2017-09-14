77fcc3ae96826c89b2340d54d7ebf6428a4211bd
shafiqRBC <sur79@live.com>
2017-09-14 11:27:11 -0400

    added config.txt & a Param to SQL

--- CopyAutoSchedule/Program.cs
+++ CopyAutoSchedule/Program.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,11 +14,14 @@ namespace CopyAutoSchedule
     {
         static string pathListFile = "";
         static int linesRead = 0, counter = 0, missingFiles = 0;
-        static string newFile = "", xmlOriginFile = "", xmlDestinFile = "", folder = Path.Combine(Application.StartupPath, @"Copied_Files");
+        static string newFile = "", xmlOriginFile = "", xmlDestinFile = "", folder = Path.Combine(Application.StartupPath, @"Copied_Files")
+            , FQDN = "SE104499.saimaple.saifg.rbc.com", database = "CentralContact", CallsForHowManyDaysBack = "-1";
+        static string[] configTxt;
         static bool copyXml = true;
 
         static void Main(string[] args)
         {
+            configFile();
             string sqlFilePath = Path.Combine(Application.StartupPath, @"sql.sql");
             string sql = File.ReadAllText(sqlFilePath );
             string connStr = connStrGet();
@@ -26,12 +30,50 @@ namespace CopyAutoSchedule
             startCopy();
         }
 
+        private static void configFile()
+        {
+            if(!File.Exists("_config.txt"))
+            {
+         
+                string configFileTxt =
+                "Server FQDN: SE104499.saimaple.saifg.rbc.com" + System.Environment.NewLine
+                + "Database Name:	CentralContact" + System.Environment.NewLine
+                + "CallsForHowManyDaysBack:	1" + System.Environment.NewLine;
+
+                File.WriteAllText("_config.txt", configFileTxt, Encoding.UTF8);
+        
+            }
+            else
+            {
+                configTxt = File.ReadAllLines("_config.txt");
+                foreach(string line in configTxt)
+                {
+                    if(Regex.Match(line, "Server FQDN").Success)
+                    {
+                        var match = Regex.Match(line, ":");
+                        FQDN = line.Substring(match.Index + 1).Trim();
+                    }
+                    if (Regex.Match(line, "Database Name").Success)
+                    {
+                        var match = Regex.Match(line, ":");
+                        database = line.Substring(match.Index + 1).Trim();
+                    }
+                    if (Regex.Match(line, "CallsForHowManyDaysBack").Success)
+                    {
+                        var match = Regex.Match(line, ":");
+                        CallsForHowManyDaysBack = "-" + line.Substring(match.Index + 1).Trim();  //add minus sign so days get substracted
+                    }
+                }
+            }
+
+        }
+
         
         private static string connStrGet()
         {
             SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
-            csb.DataSource = @"SE104499.saimaple.saifg.rbc.com";
-            csb.InitialCatalog = "CentralContact";
+            csb.DataSource = FQDN;
+            csb.InitialCatalog = database;
             csb.IntegratedSecurity = true;
             return csb.ConnectionString;
         }
@@ -40,6 +82,8 @@ namespace CopyAutoSchedule
         {
             SqlConnection con = new SqlConnection(connStr);
             SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@CallsForHowManyDaysBack", CallsForHowManyDaysBack);
+
             SqlDataReader reader;
             List<string> pathsList = new List<string>();
 
