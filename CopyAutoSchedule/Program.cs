0ed3e9b86998381f3f7209337bb95ac38e8f5fe6
shafiqRBC <sur79@live.com>
2017-09-14 15:26:47 -0400

    SqlCanBeGeneratedByAppNow

--- CopyAutoSchedule/Program.cs
+++ CopyAutoSchedule/Program.cs
@@ -16,16 +16,19 @@ namespace CopyAutoSchedule
 {
     class Program
     {
+        #region Variables
         static string pathListFile = "";
         static int linesRead = 0, counter = 0, missingFiles = 0;
         static string newFile = "", xmlOriginFile = "", xmlDestinFile = "", folder = "Copied_Files" /*folder = @"\\SE104421\h$\Test"*/
             , FQDN = "SE104499.saimaple.saifg.rbc.com", database = "CentralContact", CallsForHowManyDaysBack = "-1";
         static string[] configTxt;
         static bool copyXml = true;
+        #endregion
 
         static void Main(string[] args)
         {
             configFile();
+            sqlFileCreate();
             string sqlFilePath = Path.Combine(Application.StartupPath, @"sql.sql");
             string sql = File.ReadAllText(sqlFilePath );
             string connStr = connStrGet();
@@ -34,6 +37,7 @@ namespace CopyAutoSchedule
             startCopy();
         }
 
+       
         private static void configFile()
         {
             if(!File.Exists("_config.txt"))
@@ -88,6 +92,97 @@ namespace CopyAutoSchedule
 
         }
 
+        private static void sqlFileCreate()
+        {
+            if(!File.Exists("sql.sql"))
+            {
+#region SQL Query
+ string sqlQuery = 
+ "--Cannot declare a @var and assign tbl name to it then use that @var in FROM clause"+Environment.NewLine
+ +@"--must have WHOLE query as a string and assign to a var, then EXEC(@var); brackets must"+Environment.NewLine
+ +@Environment.NewLine
+  
+ +@"Use CentralContact;"+Environment.NewLine
+ +@"SET NOCOUNT ON;"+Environment.NewLine
+  
+ +@"declare @sql nvarchar(max), @currentMo nvarchar(2), @ifMonthChanged nvarchar(max), @oneDayAgo nvarchar(max);"+Environment.NewLine
+  
+ +@"set @currentMo = DATEPART(M, GETDATE());"+Environment.NewLine
+  
+ +@"set @oneDayAgo = convert(nvarchar , DATEADD(DAY, convert(int, @CallsForHowManyDaysBack), GETDATE()) , 25) --format 25 is '2017-01-01 00:00:00:000'"+Environment.NewLine
+  
+  
+ +@"set @ifMonthChanged ="+Environment.NewLine
+ +@"case"+Environment.NewLine
+ +@"	when month(DATEADD(DAY, convert(int, @CallsForHowManyDaysBack) , GETDATE())) = month(GETDATE())"+Environment.NewLine
+ +@"	then ' '"+Environment.NewLine
+ +@"	else ' union all  select * from dbo.sessions_month_' + cast(month(DATEADD(DAY, convert(int, @CallsForHowManyDaysBack) , GETDATE())) as nvarchar)"+Environment.NewLine
+ +@"end"+Environment.NewLine
+  
+ +@"--print @currentMo"+Environment.NewLine
+ +@"--print @oneDayAgo"+Environment.NewLine
+ +@"--print @ifMonthChanged"+Environment.NewLine
+  
+  
+ +@"--if serial#_to_ServerHostName mapping change in future, correct the CASE statement below e.g. (871001 <=> SE104421)"+Environment.NewLine
+ +@"--For production replace CASE statement with following:"+Environment.NewLine
+ +@"/*+CASE"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471002 THEN 'se441600\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471001 THEN 'se441601\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471003 THEN 'se441602\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471004 THEN 'se441603\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471005 THEN 'se441604\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471006 THEN 'se441605\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471007 THEN 'se441606\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471008 THEN 'se441607\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471009 THEN 'se441608\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471010 THEN 'se441902\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471011 THEN 'se441903\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471012 THEN 'se441904\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471013 THEN 'se441905\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471014 THEN 'se441906\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471015 THEN 'se441907\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471016 THEN 'se441908\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471017 THEN 'se441909\h$\Calls\'"+Environment.NewLine
+ +@"	  WHEN sm3.audio_module_no = 471018 THEN 'se441910\h$\Calls\'"+Environment.NewLine
+ +@"	  END*/"+Environment.NewLine
+  
+  
+ +@"set @sql = '"+Environment.NewLine
+ +@"select distinct [Paths] = ''\\''"+Environment.NewLine
+ +@"     +CASE"+Environment.NewLine
+ +@"          WHEN sm3.audio_module_no = 871001 THEN ''SE104421\h$\Calls\''"+Environment.NewLine
+ +@"          WHEN sm3.audio_module_no = 871002 THEN ''SE104422\h$\Calls\''"+Environment.NewLine
+ +@"          WHEN sm3.audio_module_no = 871003 THEN ''SE104426\h$\Calls\''"+Environment.NewLine
+ +@"          WHEN sm3.audio_module_no = 871004 THEN ''SE104427\h$\Calls\''"+Environment.NewLine
+ +@"          END+"+Environment.NewLine
+  
+ +@"        CAST(sm3.audio_module_no AS nvarchar)"+Environment.NewLine
+ +@"        + ''\''"+Environment.NewLine
+ +@"        +SUBSTRING(REPLICATE(''0'', 9-LEN(sm3.audio_ch_no))+ CAST(sm3.audio_ch_no AS varchar) ,1, 3)"+Environment.NewLine
+ +@"        + ''\''"+Environment.NewLine
+ +@"        +SUBSTRING(REPLICATE(''0'', 9-LEN(sm3.audio_ch_no))+ CAST(sm3.audio_ch_no AS varchar) ,4, 2)"+Environment.NewLine
+ +@"        + ''\''"+Environment.NewLine
+ +@"        +SUBSTRING(REPLICATE(''0'', 9-LEN(sm3.audio_ch_no))+ CAST(sm3.audio_ch_no AS varchar) ,6, 2)"+Environment.NewLine
+ +@"        + ''\''"+Environment.NewLine
+ +@"        + CAST(sm3.audio_module_no AS nvarchar) +REPLICATE(''0'', 9-LEN(sm3.audio_ch_no))+ CAST(sm3.audio_ch_no AS varchar)"+Environment.NewLine
+ +@"        + ''.wav''"+Environment.NewLine
+ +@"        --,RTRIM(sm3.start_time) as start_time"+Environment.NewLine
+ +@"        --,sm3.start_time"+Environment.NewLine
+  
+  
+ +@"from (select * from dbo.Sessions_month_'+@currentMo+@ifMonthChanged+') sm3"+Environment.NewLine
+  
+  
+  
+ +@"where sm3.start_time > '''+@oneDayAgo+'''';"+Environment.NewLine
+  
+ +@"exec (@sql);"+Environment.NewLine;
+#endregion
+                
+                File.WriteAllText("sql.sql", sqlQuery);
+            }
+        }
         
         private static string connStrGet()
         {
